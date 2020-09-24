0bc975ba0b4d1768ea3683c92227f39c38123e5e
rleon <soluciondigital15@farmaciasperuanas.pe>
2020-09-24 18:36:26 -0500

    subiendo cambios

---
+++ src/test/java/com/bolsadeideas/springboot/fom/app/SpringBootFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.fom.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
